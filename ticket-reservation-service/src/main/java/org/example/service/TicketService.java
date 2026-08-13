ddc6369be148251ccbd222756caf68d7f75bd4bb
sungsookim <fereverv@naver.com>
2026-08-13 22:05:50 +0900

    custom Pational 원복 -> Sticky partional 사용

--- ticket-reservation-service/src/main/java/org/example/service/TicketService.java
+++ ticket-reservation-service/src/main/java/org/example/service/TicketService.java
@@ -24,10 +24,10 @@ public class TicketService {
     final String stockKey = "ticket:stock:god";
 
     public ResponseEntity<String> sendToReservationTopic(TicketReservationDto requestDto) {
-        String userKey = String.format("user:%s", requestDto.getUserId());
+        String userId = String.format("user:%s", requestDto.getUserId());
 
         // 1. 중복 예약 체크
-        checkDuplicate(userKey);
+        checkDuplicate(userId);
 
         // 2. 선착순 재고 차감
         checkFirstComeFirstServed();
@@ -43,7 +43,7 @@ public class TicketService {
                     requestDto.getOrderId());
 
             // 통과 시 1차 토픽으로 고속 발행
-            kafkaTemplate.send("ticket-reservations", requestDto.getTicketId(), requestDto);
+            kafkaTemplate.send("ticket-reservations", requestDto);
 
             return ResponseEntity.ok("선착순 통과! 결제 대기열에 진입했습니다.");
         } catch (Exception e) {
@@ -51,18 +51,18 @@ public class TicketService {
 
             // 🔄 보상 트랜잭션: 카프카 전송 실패 시 Redis 재고 및 유저 상태 원복
             redisTemplate.opsForValue().increment(stockKey);
-            redisTemplate.delete(userKey);
+            redisTemplate.delete(userId);
 
             return ResponseEntity.internalServerError().body("시스템 오류로 예약 요청에 실패했습니다.");
         }
     }
 
-    private void checkDuplicate(String userKey) {
-        // 1인 1매 중복 예약 방지 (Redis SetIfAbsent)
-        Boolean isFirstRequest = redisTemplate.opsForValue().setIfAbsent(userKey, PaymentStatus.PENDING.name(), Duration.ofMinutes(10));
+    private void checkDuplicate(String userId) {
+        // 1인 1매 중복 예약 방지, userId를 redis key로 사용
+        Boolean isFirstRequest = redisTemplate.opsForValue().setIfAbsent(userId, PaymentStatus.PENDING.name(), Duration.ofMinutes(10));
 
         if (Boolean.FALSE.equals(isFirstRequest)) {
-            log.warn("[중복 예약 거부] 이미 신청한 유저입니다. 유저: {}", userKey);
+            log.warn("[중복 예약 거부] 이미 신청한 유저입니다. 유저: {}", userId);
             // ❌ return 대신 예외를 던져서 실행 흐름을 즉시 중단시킵니다.
             throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 예약을 신청하셨습니다. (1인 1매만 가능)");
         }
